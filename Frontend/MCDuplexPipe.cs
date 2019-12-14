128f3d5b859c64cfad64507b05087f12ed87cf91
Kai <kai.jellinghaus1@gmail.com>
2019-12-14 12:57:12 +0100

    Create MCDuplexPipe

---
+++ Frontend/MCDuplexPipe.cs
@@ -0,0 +1,22 @@
+﻿using System.Dynamic;
+using System.IO.Pipelines;
+
+namespace Frontend
+{
+    public sealed class MCDuplexPipe : IDuplexPipe
+    {
+        private IDuplexPipe _underlyingDuplexPipe;
+
+
+        public PipeReader Input { get; }
+
+        public PipeWriter Output { get; }
+
+        public MCDuplexPipe(IDuplexPipe pipe)
+        {
+            _underlyingDuplexPipe = pipe;
+            Input = new MCPipeReader(_underlyingDuplexPipe.Input);
+            Output = new MCPipeWriter(_underlyingDuplexPipe.Output);
+        }
+    }
+}
