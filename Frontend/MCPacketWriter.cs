5d537551ee79c7931d7076d505f3efede202ade0
Kai <kai.jellinghaus1@gmail.com>
2019-12-16 17:21:15 +0100

    Create MCPacketWriter

---
+++ Frontend/MCPacketWriter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace Frontend
+{
+    public ref struct MCPacketWriter
+    {
+        public Span<byte> Span;
+        public int Position;
+        
+        public int GetVarIntSize(int val)
+        {
+            var size = 0;
+            var v = val;
+            while ((v & -0x80) != 0)
+            {
+                v = (int)(((uint)v) >> 7);
+                size++;
+            }
+
+            return size;
+        }
+
+        public void WriteByte(byte value) 
+            => Span[Position++] = value;
+
+        public void WriteBytes(ReadOnlySpan<byte> values)
+        {
+            values.CopyTo(Span.Slice(Position));
+            Position += values.Length;
+        }
+
+        public void WriteVarInt(int value)
+        {
+            var size = 0;
+            var v = value;
+            while ((v & -0x80) != 0)
+            {
+                if (size > 5)
+                    throw new IOException("VarInt too long, its just, i can't handle that");
+
+                WriteByte((byte)(v & 0x7F | 0x80));
+                v = (int)(((uint)v) >> 7);
+                size++;
+            }
+
+            WriteByte((byte)v);
+        }
+    }
+}
