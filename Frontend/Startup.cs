48731d497773b26eb7710c92537f9a0f4dc78e7d
Kai <kai.jellinghaus1@gmail.com>
2019-12-20 19:05:38 +0100

    Comment unused Web API Code

--- Frontend/Startup.cs
+++ Frontend/Startup.cs
@@ -21,7 +21,7 @@ namespace Frontend
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        {/*
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -35,7 +35,7 @@ namespace Frontend
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
-            });
+            });*/
         }
     }
 }
