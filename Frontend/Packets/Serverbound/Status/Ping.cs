02722a090785d29cbc93f0e571899af7060f25ab
Kai <kai.jellinghaus1@gmail.com>
2019-12-23 18:03:03 +0100

    Implement Status 0x01 (Serverbound)

---
+++ Frontend/Packets/Serverbound/Status/Ping.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.Logging;
+
+namespace Frontend.Packets.Serverbound.Status
+{
+    public struct Ping : IPacket
+    {
+        public readonly int Id => 0x01;
+        public readonly  MCConnectionStage Stage => MCConnectionStage.Status;
+        public readonly bool IsServerbound => true;
+        public readonly int Size => sizeof(long);
+
+        public long Seed;
+        
+        public readonly void Write(IPacketWriter writer)
+        {
+            writer.WriteInt64(Seed);
+        }
+
+        public void Read(IPacketReader reader)
+        {
+            Seed = reader.ReadInt64();
+        }
+
+        public readonly void Process(ILogger logger, IConnectionState connectionState, IPacketQueue packetQueue)
+        {
+            packetQueue.Write(new Pong(Seed));
+        }
+    }
+}
