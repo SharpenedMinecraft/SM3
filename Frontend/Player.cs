0adf2639ae1cca5521926afdc70c8e10c36748a6
Kai <kai.jellinghaus1@gmail.com>
2019-12-29 17:57:34 +0100

    Add Player Entity

---
+++ Frontend/Player.cs
@@ -0,0 +1,15 @@
+namespace Frontend
+{
+    public class Player : IEntity
+    {
+        public Player(IEntityId id, int dimensionId)
+        {
+            Id = id;
+            DimensionId = dimensionId;
+        }
+
+        public IEntityId Id { get; }
+        
+        public int DimensionId { get; }
+    }
+}
