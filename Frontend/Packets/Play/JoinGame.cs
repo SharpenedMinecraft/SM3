a9b4ca520eb1952e8eec7e1b6e9503f7f6802c4a
Kai <kai.jellinghaus1@gmail.com>
2019-12-29 17:58:42 +0100

    Add Join Game Packet

---
+++ Frontend/Packets/Play/JoinGame.cs
@@ -0,0 +1,45 @@
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace Frontend.Packets.Play
+{
+    public readonly struct JoinGame : IWriteablePacket
+    {
+        public int Id => 0x25;
+
+        public readonly int EntityId;
+        public readonly byte GameMode;
+        public readonly int Dimension;
+        public readonly byte MaxPlayers;
+        public readonly string LevelType;
+        public readonly int ViewDistance;
+        public readonly bool ReducedDebugInfo;
+
+        public JoinGame(int entityId, byte gameMode, int dimension, byte maxPlayers, string levelType, int viewDistance, bool reducedDebugInfo)
+        {
+            EntityId = entityId;
+            GameMode = gameMode;
+            Dimension = dimension;
+            MaxPlayers = maxPlayers;
+            LevelType = levelType;
+            ViewDistance = viewDistance;
+            ReducedDebugInfo = reducedDebugInfo;
+        }
+
+        public int CalculateSize()
+            => sizeof(int) + sizeof(byte) + sizeof(int) + sizeof(byte) +
+               MCPacketWriter.GetVarIntSize(LevelType.Length) + LevelType.Length +
+               MCPacketWriter.GetVarIntSize(ViewDistance) + sizeof(bool);
+
+        public void Write(IPacketWriter writer)
+        {
+            writer.WriteInt32(EntityId);
+            writer.WriteUInt8(GameMode);
+            writer.WriteInt32(Dimension);
+            writer.WriteUInt8(MaxPlayers);
+            writer.WriteString(LevelType);
+            writer.WriteVarInt(ViewDistance);
+            writer.WriteBoolean(ReducedDebugInfo);
+        }
+    }
+}
