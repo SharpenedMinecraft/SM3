bf98a7bdf497cb9087d99afffdad6fdf664cd5a0
Kai <kai.jellinghaus1@gmail.com>
2019-12-29 12:39:00 +0100

    Update MCPacketHandler

--- Frontend/MCPacketHandler.cs
+++ Frontend/MCPacketHandler.cs
@@ -13,9 +13,11 @@ namespace Frontend
         public const string VERSION_NAME = "SM3-1.14.4";
         private readonly JsonSerializerOptions _jsonOptions;
         private readonly IPacketResolver _resolver;
+        private readonly IServiceProvider _serviceProvider;
 
-        public MCPacketHandler(ILogger<MCPacketHandler> logger, IPacketResolver resolver)
+        public MCPacketHandler(ILogger<MCPacketHandler> logger, IPacketResolver resolver, IServiceProvider serviceProvider)
         {
+            _serviceProvider = serviceProvider;
             _resolver = resolver;
             _logger = logger;
             _jsonOptions = new JsonSerializerOptions()
@@ -26,7 +28,7 @@ namespace Frontend
         
         public void HandlePacket(MCConnectionContext ctx, IPacketReader reader, IPacketQueue packetQueue, int id)
         {
-            var packet = _resolver.GetPacket(id, ctx, true);
+            var packet = _resolver.GetReadablePacket(id, ctx);
             if (packet == null)
             {
                 _logger.LogInformation($"Unknown {ctx.ConnectionStage.AsString()} Packet {id:x2}");
@@ -34,7 +36,7 @@ namespace Frontend
             }
 
             packet.Read(reader);
-            packet.Process(_logger, ctx, packetQueue);
+            packet.Process(_logger, ctx, _serviceProvider);
         }
     }
 }
