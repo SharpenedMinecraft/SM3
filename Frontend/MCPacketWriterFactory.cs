9230921e8d4632da20892b01604b221e6f60930e
Kai <kai.jellinghaus1@gmail.com>
2020-01-03 19:30:08 +0100

    Collect Packet Writer Metric

--- Frontend/MCPacketWriterFactory.cs
+++ Frontend/MCPacketWriterFactory.cs
@@ -1,12 +1,21 @@
 using System;
 using System.Buffers;
+using App.Metrics;
 
 namespace Frontend
 {
     public sealed class MCPacketWriterFactory : IPacketWriterFactory
     {
+        private readonly IMetrics _metrics;
+
+        public MCPacketWriterFactory(IMetrics metrics)
+        {
+            _metrics = metrics;
+        }
+        
         public IPacketWriter CreateWriter(Memory<byte> memory)
         {
+            _metrics.Measure.Meter.Mark(MetricsRegistry.PacketWriters);
             return new MCPacketWriter(memory, MemoryPool<byte>.Shared);
         }
     }
