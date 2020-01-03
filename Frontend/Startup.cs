8b8073c7cf1969e098f3429eb5a83fc337fd1c1a
Kai <kai.jellinghaus1@gmail.com>
2020-01-03 19:29:24 +0100

    Setup Metrics

--- Frontend/Startup.cs
+++ Frontend/Startup.cs
@@ -2,9 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using App.Metrics;
+using App.Metrics.Extensions.Configuration;
+using App.Metrics.Formatters.Ascii;
+using App.Metrics.Formatters.Json;
+using App.Metrics.Formatters.Prometheus;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +18,13 @@ namespace Frontend
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -26,7 +39,11 @@ namespace Frontend
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {/*
+        {
+            app.UseMetricsEndpoint(new MetricsPrometheusTextOutputFormatter());
+            app.UseMetricsTextEndpoint(new MetricsJsonOutputFormatter());
+            app.UseEnvInfoEndpoint();
+            /*
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
