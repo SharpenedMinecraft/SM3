c2b40bc2e04b197002f84f87938e4521c293f43c
Kai <kai.jellinghaus1@gmail.com>
2020-01-08 20:22:18 +0100

    Add ITeleportManager & TeleportManager

--- Frontend/Startup.cs
+++ Frontend/Startup.cs
@@ -39,6 +39,7 @@ namespace Frontend
             services.AddSingleton<IEntityManager, SimpleLinearEntityManager>();
             services.AddSingleton<ITagProvider, FileTagProvider>();
             services.AddSingleton<ICommandProvider, CommandProvider>();
+            services.AddSingleton<ITeleportManager, TeleportManager>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
