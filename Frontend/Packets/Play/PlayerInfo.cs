2c1df6f8bf70dafd4c7c18c82a1db930f3584c1c
Kai <kai.jellinghaus1@gmail.com>
2020-01-09 20:17:18 +0100

    Implement PlayerInfo
    Note default values

---
+++ Frontend/Packets/Play/PlayerInfo.cs
@@ -0,0 +1,61 @@
+using System;
+
+namespace Frontend.Packets.Play
+{
+    public readonly struct PlayerInfo : IWriteablePacket
+    {
+        public enum InfoType
+        {
+            AddPlayer = 0,
+            UpdateGamemode = 1,
+            UpdateLatency = 2,
+            UpdateDisplayName = 3,
+            RemovePlayer = 4
+        }
+
+        public int Id => 0x33;
+
+        public readonly InfoType Type;
+        public readonly Player[] Players;
+
+        public PlayerInfo(InfoType type, Player[] players)
+        {
+            Type = type;
+            Players = players;
+        }
+
+        public void Write(IPacketWriter writer)
+        {
+            writer.WriteVarInt((int)Type);
+            writer.WriteVarInt(Players.Length);
+
+            foreach (var player in Players)
+            {
+                writer.WriteGuid(player.Guid);
+                switch (Type)
+                {
+                    case InfoType.AddPlayer:
+                        writer.WriteString(player.Username);
+                        writer.WriteVarInt(0); // no skin support
+                        writer.WriteVarInt(1); // Gamemode 1
+                        writer.WriteVarInt(-1); // No Connection ping
+                        writer.WriteBoolean(false); // No display name
+                        break;
+                    case InfoType.UpdateGamemode:
+                        writer.WriteVarInt(1); // Gamemode 1
+                        break;
+                    case InfoType.UpdateLatency:
+                        writer.WriteVarInt(-1); // No Connection ping
+                        break;
+                    case InfoType.UpdateDisplayName:
+                        writer.WriteBoolean(false); // No display name
+                        break;
+                    case InfoType.RemovePlayer:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(Type));
+                }
+            }
+        }
+    }
+}
