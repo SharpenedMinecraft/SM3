ba7fcb79c646dd87fadd21581bc002867d97fd60
Kai <kai.jellinghaus1@gmail.com>
2020-01-11 00:37:21 +0100

    Implement Chunk & Block Position

---
+++ Frontend/ChunkPosition.cs
@@ -0,0 +1,23 @@
+namespace Frontend
+{
+    public readonly struct ChunkPosition
+    {
+        public readonly int X;
+        public readonly int Z;
+
+        public ChunkPosition(int x, int z)
+        {
+            X = x;
+            Z = z;
+        }
+
+        public void Deconstruct(out int x, out int z)
+        {
+            x = X;
+            z = Z;
+        }
+
+        public static implicit operator ChunkPosition((int x, int z) tuple) 
+            => new ChunkPosition(tuple.x, tuple.z);
+    }
+}
