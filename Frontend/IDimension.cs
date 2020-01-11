b093d8f3d0716be8f5ab3a74e9fb600198c28975
Kai <kai.jellinghaus1@gmail.com>
2020-01-11 23:50:45 +0100

    IDimension.Load returns Chunk

--- Frontend/IDimension.cs
+++ Frontend/IDimension.cs
@@ -10,7 +10,7 @@ namespace Frontend
         public Chunk? this[ChunkPosition position] => GetChunk(position);
         Chunk? GetChunk(ChunkPosition position);
 
-        void Load(ChunkPosition position);
+        Chunk Load(ChunkPosition position);
         void Unload(ChunkPosition position);
 
         IEnumerable<Chunk> GetAllChunks();
