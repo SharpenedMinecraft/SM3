3071e78b7a1f199b9a985f29c379ae77aff6e6d2
Kai <kai.jellinghaus1@gmail.com>
2020-01-12 17:48:37 +0100

    Add TagTypeName and TagType to INbtTag

--- Frontend/NbtTags.cs
+++ Frontend/NbtTags.cs
@@ -4,11 +4,15 @@ namespace Frontend
 {
     public interface INbtTag
     {
-        
+        string TagTypeName { get; }
+        byte TagType { get; }
     }
 
     public readonly struct NbtByte : INbtTag
     {
+        public string TagTypeName => "TAG_Byte";
+        public byte TagType => 1;
+        
         public readonly byte Value;
 
         public NbtByte(byte value)
@@ -19,6 +23,9 @@ namespace Frontend
 
     public readonly struct NbtShort : INbtTag
     {
+        public string TagTypeName => "TAG_Short";
+        public byte TagType => 2;
+        
         public readonly short Value;
 
         public NbtShort(short value)
@@ -29,6 +36,9 @@ namespace Frontend
 
     public readonly struct NbtInt : INbtTag
     {
+        public string TagTypeName => "TAG_Int";
+        public byte TagType => 3;
+        
         public readonly int Value;
 
         public NbtInt(int value)
@@ -39,6 +49,9 @@ namespace Frontend
 
     public readonly struct NbtLong : INbtTag
     {
+        public string TagTypeName => "TAG_Long";
+        public byte TagType => 4;
+        
         public readonly long Value;
 
         public NbtLong(long value)
@@ -49,6 +62,9 @@ namespace Frontend
 
     public readonly struct NbtFloat : INbtTag
     {
+        public string TagTypeName => "TAG_Float";
+        public byte TagType => 5;
+        
         public readonly float Value;
 
         public NbtFloat(float value)
@@ -59,6 +75,9 @@ namespace Frontend
 
     public readonly struct NbtDouble : INbtTag
     {
+        public string TagTypeName => "TAG_Double";
+        public byte TagType => 6;
+        
         public readonly double Value;
 
         public NbtDouble(double value)
@@ -69,6 +88,9 @@ namespace Frontend
 
     public readonly struct NbtByteArray : INbtTag
     {
+        public string TagTypeName => "TAG_Byte_Array";
+        public byte TagType => 7;
+        
         public readonly byte[] Value;
 
         public NbtByteArray(byte[] value)
@@ -79,6 +101,9 @@ namespace Frontend
 
     public readonly struct NbtIntArray : INbtTag
     {
+        public string TagTypeName => "TAG_Int_Array";
+        public byte TagType => 11;
+        
         public readonly int[] Value;
 
         public NbtIntArray(int[] value)
@@ -89,6 +114,9 @@ namespace Frontend
 
     public readonly struct NbtLongArray : INbtTag
     {
+        public string TagTypeName => "TAG_Long_Array";
+        public byte TagType => 12;
+        
         public readonly long[] Value;
 
         public NbtLongArray(long[] value)
@@ -99,6 +127,9 @@ namespace Frontend
 
     public readonly struct NbtString : INbtTag
     {
+        public string TagTypeName => "TAG_String";
+        public byte TagType => 8;
+        
         public readonly string Value;
 
         public NbtString(string value)
@@ -109,6 +140,9 @@ namespace Frontend
 
     public readonly struct NbtCompound : INbtTag
     {
+        public string TagTypeName => "TAG_Compound";
+        public byte TagType => 10;
+        
         public readonly ReadOnlyDictionary<string, INbtTag> Value;
 
         public NbtCompound(ReadOnlyDictionary<string, INbtTag> value)
@@ -119,6 +153,9 @@ namespace Frontend
 
     public readonly struct NbtList : INbtTag
     {
+        public string TagTypeName => "TAG_List";
+        public byte TagType => 9;
+        
         public readonly INbtTag[] Value;
 
         public NbtList(INbtTag[] value)
