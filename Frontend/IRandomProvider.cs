3b51816d09267a689df5ab2b2d0c032542393eac
Kai <kai.jellinghaus1@gmail.com>
2020-01-18 11:39:32 +0100

    Add Random Provider

---
+++ Frontend/IRandomProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace Frontend
+{
+    public interface IRandomProvider
+    {
+        long Seed { get; }
+        int Next(int bits);
+        int NextInt() => Next(32);
+    }
+}
---
+++ Frontend/JavaRandomProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Frontend
+{
+    public sealed class JavaRandomProvider : IRandomProvider
+    {
+        public long Seed { get; private set; }
+        private SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+        public int Next(int bits)
+        {
+            _semaphore.Wait();
+            try
+            {
+                Seed = ((Seed * 0x5DEECE66DL) + 0xBL) & ((1L << 48) - 1);
+                return (int) (Seed >> (48 - bits));
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+    }
+}
