8315d3a07351d97939caa09b997dcf3760bd5f03
Kai <kai.jellinghaus1@gmail.com>
2020-01-21 15:33:46 +0100

    Implement Entity Idle Packet

---
+++ Frontend/Packets/Play/EntityIdle.cs
@@ -0,0 +1,19 @@
+﻿namespace Frontend.Packets.Play
+{
+    public readonly struct EntityIdle : IWriteablePacket
+    {
+        public int Id => 0x2C;
+
+        public readonly IEntity Entity;
+
+        public EntityIdle(IEntity entity)
+        {
+            Entity = entity;
+        }
+
+        public void Write(IPacketWriter writer)
+        {
+            writer.WriteVarInt(Entity.Id);
+        }
+    }
+}
