27548f21607e89ddf37594254baa9607870fe80e
Kai <kai.jellinghaus1@gmail.com>
2020-01-21 15:35:22 +0100

    Implement Thrown Egg Entity

---
+++ Frontend/Entities/Egg.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Frontend.Entities
+{
+    public sealed class Egg : ItemedThrowable
+    {
+        public override string Type => "minecraft:egg";
+
+        public Egg(IEntityRegistry entityRegistry) : base(entityRegistry)
+        { }
+    }
+}
