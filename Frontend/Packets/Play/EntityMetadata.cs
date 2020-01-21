d72eb67163383a7dbf085c57d59027047536d998
Kai <kai.jellinghaus1@gmail.com>
2020-01-21 15:36:10 +0100

    Implement EntityMetadata Packet

---
+++ Frontend/Packets/Play/EntityMetadata.cs
@@ -0,0 +1,24 @@
+﻿using Frontend.Entities;
+
+namespace Frontend.Packets.Play
+{
+    public readonly struct EntityMetadata : IWriteablePacket
+    {
+        public int Id => 0x44;
+
+        public readonly Entity Entity;
+
+        public EntityMetadata(Entity entity)
+        {
+            Entity = entity;
+        }
+
+        public void Write(IPacketWriter writer)
+        {
+            writer.WriteVarInt(Entity.Id);
+            var metadata = new Frontend.EntityMetadata(writer);
+            Entity.WriteMetadata(metadata);
+            metadata.WriteEnd();
+        }
+    }
+}
