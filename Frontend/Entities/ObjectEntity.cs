74594fda7121899ed5b56dcb97a95b5f1a835d2e
Kai <kai.jellinghaus1@gmail.com>
2020-01-21 15:34:49 +0100

    Add Mob/Object Entity types

---
+++ Frontend/Entities/ObjectEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Frontend.Packets.Play;
+
+namespace Frontend.Entities
+{
+    public abstract class ObjectEntity : Entity
+    {
+        public override IEnumerable<IWriteablePacket> SpawnPackets
+        {
+            get
+            {
+                yield return new EntityIdle(this);
+                yield return new SpawnObject(this);
+                yield return new Packets.Play.EntityMetadata(this);
+            }
+        }
+
+        protected ObjectEntity(IEntityRegistry entityRegistry) : base(entityRegistry)
+        { }
+    }
+}
