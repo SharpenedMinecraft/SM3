02a1c858c47a21b7778f41e81f72a9c245e951f4
Kai <kai.jellinghaus1@gmail.com>
2020-01-21 15:35:00 +0100

    Add Throwable Entity abstraction

---
+++ Frontend/Entities/Throwable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Frontend.Entities
+{
+    public abstract class Throwable : ObjectEntity
+    {
+        protected Throwable(IEntityRegistry entityRegistry) : base(entityRegistry)
+        { }
+    }
+}
