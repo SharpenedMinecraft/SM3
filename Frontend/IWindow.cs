2617d877a2f953e1d2a8138925f1e8cd20060d0a
Kai <kai.jellinghaus1@gmail.com>
2020-01-22 17:25:43 +0100

    Add IWindow

---
+++ Frontend/IWindow.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Frontend
+{
+    public interface IWindow
+    {
+        byte Id { get; set; }
+        IEnumerable<IWriteablePacket> OpenPackets { get; }
+        IEnumerable<IWriteablePacket> ClosePackets { get; }
+    }
+}
