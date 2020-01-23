18c0b29aca4e9135e01fcf32e8d67bcd0c623432
Kai <kai.jellinghaus1@gmail.com>
2020-01-23 21:28:35 +0100

    Implement Generic3x3

---
+++ Frontend/Menus/Generic3x3.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Frontend.Menus
+{
+    public class Generic3x3 : BaseMenu
+    {
+        public Generic3x3(IMenuRegistry menuRegistry) : base(menuRegistry)
+        { }
+        public override string Type => "minecraft:generic_3x3";
+    }
+}
