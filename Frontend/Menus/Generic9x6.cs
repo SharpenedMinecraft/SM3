7e455a0fe5854f2651127a5185d24c499af4732f
Kai <kai.jellinghaus1@gmail.com>
2020-01-23 21:29:02 +0100

    Implement Generic9x6

---
+++ Frontend/Menus/Generic9x6.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Frontend.Menus
+{
+    public class Generic9x6 : BaseMenu
+    {
+        public Generic9x6(IMenuRegistry menuRegistry) : base(menuRegistry)
+        { }
+        public override string Type => "minecraft:generic_9x6";
+    }
+}
