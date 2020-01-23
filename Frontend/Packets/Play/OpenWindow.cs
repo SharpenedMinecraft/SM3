31d685e07fd441cb276dafe3afcae17237a01531
Kai <kai.jellinghaus1@gmail.com>
2020-01-23 21:27:12 +0100

    Implement Open Window

---
+++ Frontend/Packets/Play/OpenWindow.cs
@@ -0,0 +1,21 @@
+﻿namespace Frontend.Packets.Play
+{
+    public readonly struct OpenWindow : IWriteablePacket
+    {
+        public int Id => 0x2F;
+
+        public readonly IMenu Window;
+
+        public OpenWindow(IMenu window)
+        {
+            Window = window;
+        }
+
+        public void Write(IPacketWriter writer)
+        {
+            writer.WriteVarInt(Window.Id);
+            writer.WriteVarInt(Window.TypeId);
+            writer.WriteSpecialType(Window.Title);
+        }
+    }
+}
