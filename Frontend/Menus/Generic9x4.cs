0e64a955928ee8e032ca71c0f322dd0187a9696a
Kai <kai.jellinghaus1@gmail.com>
2020-01-23 21:28:54 +0100

    Implement Generic9x4

---
+++ Frontend/Menus/Generic9x4.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Frontend.Menus
+{
+    public class Generic9x4 : BaseMenu
+    {
+        public Generic9x4(IMenuRegistry menuRegistry) : base(menuRegistry)
+        { }
+        public override string Type => "minecraft:generic_9x4";
+    }
+}
