ff1dc88704ff07b4ee44609036af617dc7aaf687
Kai <kai.jellinghaus1@gmail.com>
2020-01-23 20:48:24 +0100

    Implement IWriteableSpecial type on Chat

--- Frontend/Chat.cs
+++ Frontend/Chat.cs
@@ -1,9 +1,16 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ServiceModel.Channels;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Frontend
 {
-    public sealed class Chat
+    public sealed class Chat : IWriteableSpecialType
     {
+        private static JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions()
+        {
+            IgnoreNullValues = true
+        };
+        
         [JsonPropertyName("text")] public string? Text { get; set; }
         [JsonPropertyName("bold")] public bool? Bold { get; set; }
         [JsonPropertyName("italic")] public bool? Italic { get; set; }
@@ -13,5 +20,13 @@ namespace Frontend
         [JsonPropertyName("color")] public string? Color { get; set; }
         [JsonPropertyName("insertion")] public string? Insertion { get; set; }
         [JsonPropertyName("extra")] public Chat[]? Extra { get; set; }
+        
+        public void Write(IPacketWriter writer)
+        {
+            var data = JsonSerializer.SerializeToUtf8Bytes(this, _jsonSerializerOptions);
+            
+            writer.WriteVarInt(data.Length);
+            writer.WriteBytes(data);
+        }
     }
 }
