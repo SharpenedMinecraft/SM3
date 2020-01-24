e3c6fee1156dbe67090508a1442a1b69322f9f86
Kai <kai.jellinghaus1@gmail.com>
2020-01-24 21:56:42 +0100

    Make ItemStack.Item nullable

--- Frontend/ItemStack.cs
+++ Frontend/ItemStack.cs
@@ -1,21 +1,28 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 
 namespace Frontend
 {
     [StructLayout(LayoutKind.Sequential)]
     public readonly struct ItemStack<T>
-        where T : notnull, IItem
+        where T : class, IItem
     {
-        public readonly T Item { get; }
+        public readonly T? Item { get; }
         public readonly sbyte Count { get; }
 
-        public ItemStack(T item, sbyte count)
+        public ItemStack(T? item, sbyte count)
         {
             Item = item;
             Count = count;
         }
-        
+
+        public void Deconstruct(out T? item, out sbyte count)
+        {
+            item = Item;
+            count = Count;
+        }
+
         public NetworkSlot NetworkSlot => new NetworkSlot(Count != 0, Item.TypeId, Count, null);
     }
 }
