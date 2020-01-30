8279db9759c6805e1d2f7677c6019993218270ea
Tobi406 <tobim406@gmail.com>
2020-01-30 19:31:23 +0100

    Use two different classes. Client and Serverbound. Now supports that messages that have been sent to chat can be read in chat.

---
+++ Frontend/Packets/Play/ClientboundChatMessage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Frontend.Packets.Play
+{
+    public readonly struct ClientboundChatMessage : IWriteablePacket
+    {
+        private static JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions()
+        {
+            IgnoreNullValues = true
+        };
+        public int Id => 0x0F;
+
+        public readonly Chat Chat;
+        public readonly byte Position;
+
+        public ClientboundChatMessage(Chat chat, byte position)
+        {
+            Chat = chat;
+            Position = position;
+        }
+
+        public void Write(IPacketWriter writer)
+        {
+            var data = JsonSerializer.SerializeToUtf8Bytes(Chat, _jsonSerializerOptions);
+
+            writer.WriteVarInt(data.Length);
+            writer.WriteBytes(data);
+            writer.WriteUInt8(Position);
+        }
+       
+    }
+}
